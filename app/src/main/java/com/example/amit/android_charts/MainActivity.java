0e4b8396d84f615fa8b635fd15f3d888617fc9fe
amitgunjal26 <amitgunjal26@gmail.com>
2016-06-01 18:06:52 +0530

    Final Commit for this Module

--- app/src/main/java/com/example/amit/android_charts/MainActivity.java
+++ app/src/main/java/com/example/amit/android_charts/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.amit.android_charts;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -28,8 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-
+// *****************************************Horizontal Bar Chart***************
         HorizontalBarChart barChart = (HorizontalBarChart) findViewById(R.id.chart);
+
         ArrayList<BarEntry> entries = new ArrayList<>();
         entries.add(new BarEntry(4f, 0));
         entries.add(new BarEntry(8f, 1));
@@ -39,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         entries.add(new BarEntry(9f, 5));
 
         BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
 
         ArrayList<String> labels = new ArrayList<String>();
         labels.add("January");
@@ -51,9 +59,32 @@ public class MainActivity extends AppCompatActivity {
         BarData data = new BarData(labels, dataset);
         barChart.setData(data);
 
-        barChart.setDescription("Number of tweets");
+        //barChart.setDescription("Number of tweets");
         dataset.setColors(ColorTemplate.COLORFUL_COLORS);
         barChart.animateY(5000);
+
+        //*******************************Pie hart***********************************
+
+
+        PieChart pieChart = (PieChart) findViewById(R.id.chart1);
+
+        ArrayList<Entry> entries1 = new ArrayList<>();
+        entries1.add(new Entry(4f, 0));
+        entries1.add(new Entry(8f, 1));
+        entries1.add(new Entry(6f, 2));
+        entries1.add(new Entry(12f, 3));
+        entries1.add(new Entry(18f, 4));
+        entries1.add(new Entry(9f, 5));
+        PieDataSet dataset1 = new PieDataSet(entries1, "# of Calls");
+
+        PieData data1 = new PieData(labels, dataset1); // initialize Piedata
+        pieChart.setData(data1); //set data into chart
+
+        //pieChart.setDescription("Description");
+
+        dataset1.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieChart.animateY(5000);
+
     }
 
     @Override
